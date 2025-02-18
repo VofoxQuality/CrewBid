feeb2132b8e77a10d6cfdd2c2cf9e7bac852dcaa
VOFOX\nimishilabn <nimishilabh@vofoxsolutions.com>
2025-02-18 11:54:36 +0530

    Login Check

--- src/test/java/testCases/LoginTest.java
+++ src/test/java/testCases/LoginTest.java
@@ -19,7 +19,7 @@ public class LoginTest extends WbidBasepage{
 	
   @Test(priority = 1, enabled = true)
   public void CBW002001000001() {
-	 logger = WbidBasepage.extent.createTest(" LOGIN PAGE (MTS001001000001)").assignAuthor("VS/483") ;
+	 logger = WbidBasepage.extent.createTest(" LOGIN PAGE (CBW002001000001)").assignAuthor("VS/483") ;
 	 logger.info("To check that CrewBid Login page is displaying when entering the Url");
 		actualtitle = objpage.pageHeading();
 		expectedtitle = "CrewbidWebApp";
@@ -30,20 +30,20 @@ public class LoginTest extends WbidBasepage{
   }
   @Test(priority = 2, enabled = true)
   public void CBW002001000002() {
-	 logger = WbidBasepage.extent.createTest(" LOGIN PAGE (MTS001001000002)").assignAuthor("VS/483") ;
+	 logger = WbidBasepage.extent.createTest(" LOGIN PAGE (CBW002001000002)").assignAuthor("VS/483") ;
 	 logger.info("To check that Employee Number Text Box");
 	 Assert.assertTrue(objpage.visibilityofusernamebox(), "Employee Number Textbox Not visible");
   }
   @Test(priority = 3, enabled = false)
   public void CBW002001000003() {
-	 logger = WbidBasepage.extent.createTest(" LOGIN PAGE (MTS001001000003)").assignAuthor("VS/483") ;
+	 logger = WbidBasepage.extent.createTest(" LOGIN PAGE (CBW002001000003)").assignAuthor("VS/483") ;
 	 logger.info("To check that Password Text Box");
 	 Assert.assertTrue(objpage.visibilityofpasswordbox(), "Password Textbox Not visible");
 
   }
   @Test(priority = 4, enabled = true)
   public void CBW002001000004() {
-	 logger = WbidBasepage.extent.createTest(" LOGIN PAGE (MTS001001000004)").assignAuthor("VS/483") ;
+	 logger = WbidBasepage.extent.createTest(" LOGIN PAGE (CBW002001000004)").assignAuthor("VS/483") ;
 	 logger.info("To check Login functionality");
 	 objpage.login();
 	 objwait.waitS(9000);
