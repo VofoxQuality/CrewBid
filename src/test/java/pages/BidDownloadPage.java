301e3e3f886000b82cdba026010a793bc00d9a5c
VOFOX\nimishilabn <nimishilabh@vofoxsolutions.com>
2025-02-19 21:48:51 +0530

    Bid Download upto 22Tc

--- src/test/java/pages/BidDownloadPage.java
+++ src/test/java/pages/BidDownloadPage.java
@@ -1,5 +1,9 @@
 package pages;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -7,6 +11,7 @@ import org.openqa.selenium.support.PageFactory;
 
 import utilities.ActionUtilities;
 import utilities.WaitCondition;
+import utilities.WbidBasepage;
 
 public class BidDownloadPage {
 	WebDriver driver;
@@ -38,13 +43,167 @@ public class BidDownloadPage {
 
 	@FindBy(xpath = "//a[text()=\"Retrieve New BidData\"]")
 	public WebElement retrievenewbiddata;
-	@FindBy(xpath="//a[text()=\"Retrieve Historical BidData\"]")
+	@FindBy(xpath = "//a[text()=\"Retrieve Historical BidData\"]")
 	public WebElement retrievehistoricalbiddata;
 
 	public boolean fordisplaynewbiddata() {
-		return objaction.fordisplay(retrievenewbiddata)&& objaction.fordisplay(retrievehistoricalbiddata);
+		return objaction.fordisplay(retrievenewbiddata) && objaction.fordisplay(retrievehistoricalbiddata);
 	}
-	@FindBy(xpath="")
-	public WebElement oopsOkbtn;
 
+	// TC 7
+	public void forclicknewbiddata() {
+		objaction.click(retrievenewbiddata);
+	}
+
+	@FindBy(xpath = "//h2[text()=\"Enter Employee Number\"]")
+	public WebElement empnumberpopupheader;
+
+	public String checkEmpnumpopupheader() {
+		return objaction.gettext(empnumberpopupheader);
+	}
+
+	// TC 8
+	@FindBy(xpath = "(//p[@class=\"text-center\"])[2]")
+	public WebElement emppopuptext;
+
+	public String checkempPopupText() {
+		return objaction.gettext(emppopuptext);
+	}
+
+	// TC 9
+	@FindBy(xpath = "//button[@class=\"submit-cancel btn\"]")
+	public WebElement cancel_btn;
+	@FindBy(xpath = "//button[@class=\"submit-bid btn\"]")
+	public WebElement ok_btn;
+
+	public boolean fordisplaybtninpopup() {
+		return objaction.fordisplay(cancel_btn) && objaction.fordisplay(ok_btn);
+	}
+
+	// Tc 10
+	@FindBy(name = "empNum")
+	public WebElement empplaceholder;
+
+	public boolean checkplaceholder() {
+		String att = objaction.getAttribute(empplaceholder, "placeholder");
+		WbidBasepage.logger.info(att);
+		if (att.contains("Employee No:")) {
+			WbidBasepage.logger.pass("Placeholder is same");
+			return true;
+		} else {
+			WbidBasepage.logger.fail("Placeholder mismatch");
+			return false;
+		}
+	}
+
+	// TC 11
+	public void enterempid() {
+		objaction.sendkey(empplaceholder, "21221");
+		objaction.click(ok_btn);
+	}
+
+	public void forclickokbtn() {
+		objaction.click(ok_btn);
+	}
+
+	@FindBy(xpath = "//h2[text()=\"Retrieve New Bid Period\"]")
+	public WebElement retrievebidpopupheader;
+
+	public String checkretrievebidpopupheader() {
+		return objaction.gettext(retrievebidpopupheader);
+	}
+
+	// TC 13
+	@FindBy(xpath = "//*[@id=\"newBidModal\"]/div/div/div[2]/div/div[1]")
+	public List<WebElement> labels;
+	public List<String> Actuallabels;
+
+	public boolean checkRetrievebidpopuplabels() {
+		Actuallabels = new ArrayList<>();
+		for (WebElement option : labels) {
+			String label = objaction.gettext(option);
+			WbidBasepage.logger.info("Labels:" + label);
+			Actuallabels.add(label);
+		}
+		List<String> Expectedlabels = Arrays.asList("Base:", "Position:", "Round:", "Month:");
+		if (Actuallabels.containsAll(Expectedlabels) && Expectedlabels.containsAll(Actuallabels)) {
+			WbidBasepage.logger.pass("Labels are same");
+			return true;
+		} else {
+			WbidBasepage.logger.fail("Labels are mismatch");
+			return false;
+		}
+	}
+
+	// TC 15
+	@FindBy(xpath = "(//div[@class=\"modal-header text-center\"]/button[@class=\"close\"])[1]")
+	public WebElement close_btn;
+
+	public void click_closebtn() {
+		objaction.click(close_btn);
+	}
+
+//TC16
+	@FindBy(xpath = "//*[@id=\"newBidModal\"]/div/div/div[2]/div[1]/div[2]/button")
+	public List<WebElement> basecities;
+
+	public boolean checkcities_isenable() {
+		boolean allEnabled = true;
+		WbidBasepage.logger.info("Verifying all base cities:");
+
+		for (WebElement option : basecities) {
+			String cityName = option.getText();
+			WbidBasepage.logger.info("City: " + cityName);
+
+			objwait.waitForElemntTobeClickable(driver, option, 5);
+			objaction.click(option);
+			objwait.waitS(3000);
+			String att = objaction.getAttribute(option, "class");
+
+			if (att.contains("active-re")) {
+				WbidBasepage.logger.pass("City '" + cityName + "' is enabled.");
+			} else {
+				WbidBasepage.logger.fail("City '" + cityName + "' is disabled.");
+				allEnabled = false;
+			}
+		}
+
+		if (allEnabled) {
+			WbidBasepage.logger.pass("✅ All 14 base cities are enabled.");
+		} else {
+			WbidBasepage.logger.fail("❌ Some base cities are disabled.");
+		}
+		return allEnabled;
+	}
+//TC17
+	@FindBy(xpath="//*[@id=\"newBidModal\"]/div/div/div[2]/div[2]/div[2]/button")
+	public List<WebElement> positionlist;
+	public boolean checkposition_isenable() {
+		boolean allEnabled = true;
+		WbidBasepage.logger.info("Verifying all Positions:");
+
+		for (WebElement option : positionlist) {
+			String position = option.getText();
+			WbidBasepage.logger.info("City: " + position);
+
+			objwait.waitForElemntTobeClickable(driver, option, 5);
+			objaction.click(option);
+			objwait.waitS(3000);
+			String att = objaction.getAttribute(option, "class");
+
+			if (att.contains("active-re")) {
+				WbidBasepage.logger.pass("Position'" + position + "' is enabled.");
+			} else {
+				WbidBasepage.logger.fail("Position '" + position + "' is disabled.");
+				allEnabled = false;
+			}
+		}
+
+		if (allEnabled) {
+			WbidBasepage.logger.pass("✅ All 3 Positions are enabled.");
+		} else {
+			WbidBasepage.logger.fail("❌ Some Positions are disabled.");
+		}
+		return allEnabled;
+	}
 }
--- src/test/java/testCases/BidDownloadTest.java
+++ src/test/java/testCases/BidDownloadTest.java
@@ -15,7 +15,7 @@ public class BidDownloadTest extends WbidBasepage {
 	LoginPage objlogin = new LoginPage(driver);
 	WaitCondition objwait = new WaitCondition();
 	ActionUtilities objaction = new ActionUtilities(driver);
-	BidDownloadPage objdownload=new BidDownloadPage(driver);
+	BidDownloadPage objdownload = new BidDownloadPage(driver);
 	protected String actualtitle;
 	protected String expectedtitle;
 
@@ -34,20 +34,20 @@ public class BidDownloadTest extends WbidBasepage {
 	public void CBW003001000002() {
 		logger = extent.createTest(" BID DATA DOWNLOAD (CBW003001000001)").assignAuthor("VS/482");
 		logger.info("Verify the update popup");
-		
+
 	}
 
 	@Test(priority = 3, enabled = false)
 	public void CBW003001000003() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000003)").assignAuthor("VS/482");
-	    logger.info("Verify the subscription expired");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000003)").assignAuthor("VS/482");
+		logger.info("Verify the subscription expired");
 	}
 
 	@Test(priority = 4, enabled = true)
 	public void CBW003001000004() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000004)").assignAuthor("VS/482");
-	    logger.info("Verify the login (subscribed user)");
-	    objlogin.login();
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000004)").assignAuthor("VS/482");
+		logger.info("Verify the login (subscribed user)");
+		objlogin.login();
 		objwait.waitS(9000);
 		logger.info("Assert the title : \"Crewbid\" in the top left");
 		Assert.assertTrue(objdownload.fordisplaylogo(), "Logo not display");
@@ -55,113 +55,149 @@ public class BidDownloadTest extends WbidBasepage {
 
 	@Test(priority = 5, enabled = true)
 	public void CBW003001000005() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000005)").assignAuthor("VS/482");
-	    logger.info("Verify the Retreive button");
-	    Assert.assertTrue(objdownload.fordisplayretrivedropdown(),"Retrive button not displayed");
-	    logger.info("Assert : Retrieve new bid data and Retrieve Historical bid data");
-	    objdownload.click_retrievedownload();
-	    Assert.assertTrue(objdownload.fordisplaynewbiddata(),"Retrieve new bid data and Retrieve Historical bid data are not displayed in dropdown");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000005)").assignAuthor("VS/482");
+		logger.info("Verify the Retreive button");
+		Assert.assertTrue(objdownload.fordisplayretrivedropdown(), "Retrive button not displayed");
+		logger.info("Assert : Retrieve new bid data and Retrieve Historical bid data");
+		objdownload.click_retrievedownload();
+		Assert.assertTrue(objdownload.fordisplaynewbiddata(),
+				"Retrieve new bid data and Retrieve Historical bid data are not displayed in dropdown");
 	}
 
 	@Test(priority = 6, enabled = true)
 	public void CBW003001000006() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000006)").assignAuthor("VS/482");
-	    logger.info("");
-	}
-
-	@Test(priority = 7, enabled = true)
-	public void CBW003001000007() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000007)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000006)").assignAuthor("VS/482");
+		logger.info("Verify the user able to select Retrieve new bid data button");
+		objdownload.forclicknewbiddata();
+		Assert.assertEquals(objdownload.checkEmpnumpopupheader(), "Enter Employee Number",
+				"Header mismatch or not displayed");
+		logger.info("Assert :  title in the new popup Alert");
 	}
 
 	@Test(priority = 8, enabled = true)
-	public void CBW003001000008() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000008)").assignAuthor("VS/482");
-	    logger.info("");
+	public void CBW003001000007() {
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000007)").assignAuthor("VS/482");
+		logger.info("Verify the Content correct in the \"Enter emplyoee number \" popup alert");
+		Assert.assertEquals(objdownload.checkempPopupText(),
+				"Please enter the employee number for whom the bid will be submitted (no 'e').", "Text is mismatch");
+		logger.info("Assert: \"Please enter the employee number for whom the bid will be submitted (no 'e').\"");
 	}
 
 	@Test(priority = 9, enabled = true)
-	public void CBW003001000009() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000009)").assignAuthor("VS/482");
-	    logger.info("");
+	public void CBW003001000008() {
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000008)").assignAuthor("VS/482");
+		logger.info("Verify the Cancel button and OK button in the popup alert");
+		Assert.assertTrue(objdownload.fordisplaybtninpopup(), "Buttons not displayed");
+		logger.info("Assert : Cancel button and OK button");
 	}
 
 	@Test(priority = 10, enabled = true)
-	public void CBW003001000010() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000010)").assignAuthor("VS/482");
-	    logger.info("");
+	public void CBW003001000009() {
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000009)").assignAuthor("VS/482");
+		logger.info("Verify the user ID field with content \"Employee No:\"");
+		Assert.assertTrue(objdownload.checkplaceholder(), "Placeholder Mismatch or not displayed");
+		logger.info("Assert: \"Employee No:\" content in the field");
 	}
 
 	@Test(priority = 11, enabled = true)
-	public void CBW003001000011() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000011)").assignAuthor("VS/482");
-	    logger.info("");
+	public void CBW003001000010() {
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000010)").assignAuthor("VS/482");
+		logger.info("Verify the User ID ");
+		objdownload.enterempid();
+		Assert.assertEquals(objdownload.checkretrievebidpopupheader(), "Retrieve New Bid Period",
+				"Popup header mismatch or popup not displayed");
+		logger.info("Assert : Rediret to Retreive new bid perid popup page");
 	}
 
 	@Test(priority = 12, enabled = true)
+	public void CBW003001000011() {
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000011)").assignAuthor("VS/482");
+		logger.info("Verify the title in the Retrive new bid period popup alert");
+		Assert.assertEquals(objdownload.checkretrievebidpopupheader(), "Retrieve New Bid Period",
+				"Popup header mismatch or popup not displayed");
+		logger.info("Assert :Title");
+	}
+
+	@Test(priority = 7, enabled = true)
 	public void CBW003001000012() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000012)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000012)").assignAuthor("VS/482");
+		logger.info("verify the Retreive new bid data title");
+		Assert.assertEquals(objdownload.checkEmpnumpopupheader(), "Enter Employee Number",
+				"Header mismatch or not displayed");
+		logger.info("Assert : Enter employee number title in the new popup Alert");
 	}
 
 	@Test(priority = 13, enabled = true)
 	public void CBW003001000013() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000013)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000013)").assignAuthor("VS/482");
+		logger.info("Verify the title Base, Position , Round, MOnth  heading appers with double collon");
+		Assert.assertTrue(objdownload.checkRetrievebidpopuplabels(), "Labels mismatch");
+		logger.info("Assert : Subtitle : Base,Position , Round, MOnth  heading appers with double collon");
 	}
 
-	@Test(priority = 14, enabled = true)
+	@Test(priority = 14, enabled = false)
 	public void CBW003001000014() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000014)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000014)").assignAuthor("VS/482");
+		logger.info("Verify the server sresponse");
 	}
 
 	@Test(priority = 15, enabled = true)
 	public void CBW003001000015() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000015)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000015)").assignAuthor("VS/482");
+		logger.info("To check the close button");
+		objdownload.click_closebtn();
+		Assert.assertEquals(objdownload.checkEmpnumpopupheader(), "Enter Employee Number",
+				"Header mismatch or not displayed");
+		logger.info("Assert : close button \n" + "Once click on the close button popup should be close");
+
 	}
 
 	@Test(priority = 16, enabled = true)
 	public void CBW003001000016() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000016)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000016)").assignAuthor("VS/482");
+		logger.info("Verify the all the base is enabled");
+		objdownload.forclickokbtn();
+		objwait.waitS(3000);
+		Assert.assertTrue(objdownload.checkcities_isenable(), "Base not enable");
+		logger.info("Assert : All the base cities");
 	}
 
 	@Test(priority = 17, enabled = true)
 	public void CBW003001000017() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000017)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000017)").assignAuthor("VS/482");
+		logger.info("To check the positions are enabled state");
+		Assert.assertTrue(objdownload.checkposition_isenable(), "Positions are disable");
+		logger.info("Assert : CP , FO and FA");
 	}
 
 	@Test(priority = 18, enabled = true)
 	public void CBW003001000018() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000018)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000018)").assignAuthor("VS/482");
+		logger.info("");
 	}
 
 	@Test(priority = 19, enabled = true)
 	public void CBW003001000019() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000019)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000019)").assignAuthor("VS/482");
+		logger.info("");
 	}
 
 	@Test(priority = 20, enabled = true)
 	public void CBW003001000020() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000020)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000020)").assignAuthor("VS/482");
+		logger.info("");
 	}
+
 	@Test(priority = 21, enabled = true)
 	public void CBW003001000021() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000021)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000021)").assignAuthor("VS/482");
+		logger.info("");
 	}
 
 	@Test(priority = 22, enabled = true)
 	public void CBW003001000022() {
-	    logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000022)").assignAuthor("VS/482");
-	    logger.info("");
+		logger = extent.createTest("BID DATA DOWNLOAD (CBW003001000022)").assignAuthor("VS/482");
+		logger.info("");
 	}
 
 }
